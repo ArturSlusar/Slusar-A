29d5606454ac92d5562a449ceca3b146299a86e4
ArturSlusar <artursusar@gmail.com>
2026-03-20 11:16:43 +0500

    Time: 11 ms (65.66%) | Memory: 32.2 MB (43.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,14 +1,14 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        see = set()
+        seen = set()
 
         for num in nums:
             #Если число уже бвло добавлено то это дупликат
-            if num in see:
+            if num in seen:
                 return True
 
             #Если числа не юыло то добавлем его а хэш
-            see.add(num)
+            seen.add(num)
 
         #Если цикл завершился полностью то значит дупликатов нету
         return False 
