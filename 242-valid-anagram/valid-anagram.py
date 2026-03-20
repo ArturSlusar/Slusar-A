b0d6d1b7957b6809d633bf8a810aec3e814532c6
ArturSlusar <artursusar@gmail.com>
2026-03-20 10:12:48 +0500

    Time: 11 ms (76.01%) | Memory: 19.4 MB (67.05%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #Если данные отличаются то это точно не анаграммы
+        if len(s) != len(t):
+            return False
+
+        counts = {}
+
+        #Считаем буквы
+        for char in s:
+            if char in counts:
+                counts[char] += 1 #Если буква есть то счетчик увеличивается
+            else:
+                counts[char] = 1 #Если нет то добавляем со значением 1
+
+        #Проверяем следующую строку вычитая буквы из словаря
+        for char in t:
+            #Если букв вообще не было в первой строку это не анаграмм
+            if char not in counts:
+                return False
+
+            counts[char] -= 1
+
+            if counts[char] < 0:
+                return False
+
+        return True
+
+            
+        
