20d8f0b1edc20c87ed2b0fdde9d5b52df63a89eb
ArturSlusar <artursusar@gmail.com>
2026-03-24 16:28:29 +0500

    Time: 1 ms (60.65%) | Memory: 20.6 MB (35.05%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,18 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #создвем хэш таблицу для хранения уже просмотренных чисел ключ которых это само числоо а знпчение это его индекс в массиве
-        see = {}
+        seen = {}
 
-        #Проходим по массиву получая индекс i и значение num
         for i in range(len(nums)):
             num = nums[i]
             #Ищем число котороые в сумме с num выдаст target
             need = target - num
             #Проверяем есть ли это число в нашей хэш таблице
-            if need in see:
-                return [see[need], i]
-            see[num] = i
+            if need in seen:
+                return [seen[need], i]
+            seen[num] = i
 
         return[]
 
-        
